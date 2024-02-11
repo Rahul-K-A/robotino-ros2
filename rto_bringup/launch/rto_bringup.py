5542d0509e0a943eaf435da79e71b4862a26e817
Rahul-K-A <karahul209@gmail.com>
2024-02-11 11:40:14 +0530

    Create bringup.py

---
+++ rto_bringup/launch/rto_bringup.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.actions.set_parameter import SetParameter
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument("hostname",
+                               default_value = "172.26.1.1",
+                               description="Robotino IP Address"),
+        Node(
+            package='rto_node',
+            namespace='rto3',
+            executable='rto_node',
+            name='rto_core',
+            parameters=[{'hostname': LaunchConfiguration("hostname")}]
+        ),
+        Node(
+            package='rto_node',
+            namespace='rto3',
+            executable='rto_laserrangefinder_node',
+            name='rto_laser',
+            parameters=[{'hostname': LaunchConfiguration("hostname")}]
+        ),
+    ])
